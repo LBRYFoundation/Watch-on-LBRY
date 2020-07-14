cc2d4f41ebfede83b96efa5fb85462daf2d20125
kodxana <romanrex19@int.pl>
2020-07-14 13:27:58 +0200

    Added new button to popup

--- popup/popup.js
+++ popup/popup.js
@@ -9,7 +9,7 @@ chrome.storage.local.get(['enabled', 'redirect'], ({ enabled, redirect }) => {
 
   const currentButton = enabled ? enable : disable;
   currentButton.classList.add('active');
-  
+
   const currentRadio = !redirect ? lbrySite : redirect === 'lbry.tv' ? lbrySite : lbryApp;
   currentRadio.classList.add('active');
 });
@@ -18,7 +18,7 @@ const checkElementForClass = (elToAdd, elToRemove) => {
   if(!elToAdd.classList.contains('active')){
 
     elToAdd.classList.add('active');
-    elToRemove.classList.remove('active'); 
+    elToRemove.classList.remove('active');
   }
 }
 
@@ -27,12 +27,12 @@ const attachClick = (selector, handler) =>{
     const element = event.target;
     const name = event.target.getAttribute('name');
     const value = event.target.getAttribute('value');
-    typeof handler==='function' ? handler(element, name, value): null; 
+    typeof handler==='function' ? handler(element, name, value): null;
   });
 }
 
 attachClick('.enable', (element, name, value) => {
-  const parsedValue = !!+value; 
+  const parsedValue = !!+value;
   if(name){
     checkElementForClass(element, name === 'enable' ? disable : enable  );
     chrome.storage.local.set({ enabled: parsedValue });
@@ -44,4 +44,9 @@ attachClick('.redirect', (element, name, value) => {
     checkElementForClass(element, name === 'site' ? lbryApp : lbrySite);
     chrome.storage.local.set({ redirect: value });
   }
+});
+
+var button = document.getElementById("btn1");
+button.addEventListener("click", function(){
+    chrome.tabs.create({url:"/tools/YTtoLBRY.html"});
 });
