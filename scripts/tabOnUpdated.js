555898a55132a11a5ac6765c5c779100f777996e
Clayton Hickey <clayton@claytondoesthings.xyz>
2020-07-24 16:25:09 -0400

    lbry.tv links now redirect to lbry app

--- scripts/tabOnUpdated.js
+++ scripts/tabOnUpdated.js
@@ -2,6 +2,46 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   chrome.storage.local.get(async ({ enabled }) => {
     if (!enabled) return;
     if (!changeInfo.url) return;
+    if (tab.url.match(/\b(https:\/\/lbry.tv|lbry:\/\/)/g)) {
+      chrome.storage.local.get('redirect', ({redirect}) => {
+        var redirectTo;
+
+        // If handler can be added for new lbry:// tabs, below code can be uncommented and merged
+
+        // if (redirect === "lbry.tv") {
+        //   console.error(tab.url);
+        //   let isChannel = tab.url.match(/^lbry:\/\/@[^^$#@;/"<>%{}|^~[\]`]+?#([a-z0-9]{40}|[a-z0-9])$/g);
+        //   let isClaim = tab.url.match(/^lbry:\/\/@[^^$#@;/"<>%{}|^~[\]`]+?#([a-z0-9]{40}|[a-z0-9])\/[^^$#@;/"<>%{}|^~[\]`]+?#([a-z0-9]{40}|[a-z0-9])$/g);
+
+        //   if (isChannel) {
+        //     let channelId = tab.url.match(/#[a-z0-9]+/g)[0].substr(1);
+        //     redirectTo = `https://lbry.tv/${tab.url.match(/@[^^$#@;/"<>%{}|^~[\]`]+?(?=#)/g)[0]}${channelId.length > 1 ? '#' : ':'}${channelId}`;
+        //   } else if (isClaim) {
+        //     let channelId = tab.url.match(/#[a-z0-9]+/g)[0].substr(1);
+        //     let claimId = tab.url.match(/[a-z0-9]+$/g)[0];
+        //     redirectTo = `https://lbry.tv/${tab.url.match(/@[^^$#@;/"<>%{}|^~[\]`]+?(?=#)/g)[0]}${channelId.length > 1 ? '#' : ':'}${channelId}/${tab.url.match(/[^^$#@;/"<>%{}|^~[\]`]+(?=#[a-z0-9]+$)/g)}${claimId.length > 1 ? '#' : ':'}${claimId}`;
+        //   }
+        //   console.error(redirectTo);
+        // }
+
+        if (redirect === "app") {
+          let isChannel = tab.url.match(/^(https|http):\/\/lbry.tv\/@([^$#@;/"<>%{}|^~[\]`])+?(#([a-z0-9]{40})|:[a-z0-9])$/g);
+          let isClaim =  tab.url.match(/^(https|http):\/\/lbry.tv\/@([^$#@;/"<>%{}|^~[\]`])+?(#([a-z0-9]{40})|:[a-z0-9])\/([^$#@;/"<>%{}|^~[\]`])+?(#([a-z0-9]{40})|:[a-z0-9])$/g);
+
+          if (isChannel) {
+            redirectTo = `lbry://${tab.url.match(/@([^$#@;/"<>%{}|^~[\]`])+?(?=[#:])/g)[0]}#${tab.url.match(/#([a-z0-9]{40})|:[a-z0-9]$/g)[0].substr(1)}`;
+          } else if (isClaim) {
+            redirectTo = `lbry://${tab.url.match(/@([^$#@;/"<>%{}|^~[\]`])+?(?=[#:])/g)}#${tab.url.match(/(#([a-z0-9]{40})|:[a-z0-9])(?=\/([^$#@;/"<>%{}|^~[\]`])+?(#([a-z0-9]{40})|:[a-z0-9])$)/g)[0].substr(1)}${tab.url.match(/\/([^$#@;/"<>%{}|^~[\]`])+?(?=[#:])/g)[0]}#${tab.url.match(/(#([a-z0-9]{40})|:[a-z0-9])$/g)[0].substr(1)}`;
+          }
+        }
+
+        if (redirectTo) {
+          chrome.tabs.update(tabId, { url: redirectTo + "?src=watch-on-lbry" });
+          if (redirect === "app") alert("Opened link in LBRY App!");
+        }
+      });
+      return;
+    }
     const { id, type } = getId(tab.url);
     if (!id) return;
 
