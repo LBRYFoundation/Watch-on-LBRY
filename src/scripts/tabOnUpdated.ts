9f8e521fa60165677e32983b8aae3a4d1d07692c
Kevin Raoofi <nullindev@gmail.com>
2020-10-13 04:37:13 -0400

    URL parsing for app redirects
    URL parsing logic is basically a straight port of how lbryio/lbry-sdk
    parses lbry URLs. While there's a lot more going on, it should be
    easier to follow what they're doing this way.
    
    Also, added some light unit tests for the URL parsing using jest.
    Which, of course, needed a babel config.
    
    With this, tabOnUpdated was able to be rewritten more elegantly to
    handle redirects as it delegates a lot of the work to utilities now.

--- src/scripts/tabOnUpdated.ts
+++ src/scripts/tabOnUpdated.ts
@@ -1,54 +1,43 @@
-import { getSettingsAsync, redirectDomains } from "../common/settings";
-import { ytService } from "../common/yt";
+import { appRedirectUrl, parseProtocolUrl } from '../common/lbry-url';
+import { getSettingsAsync, redirectDomains } from '../common/settings';
+import { ytService } from '../common/yt';
+
+function openApp(tabId: number, url: string) {
+  chrome.tabs.update(tabId, { url });
+  alert('Opened link in LBRY App!'); // Better for UX since sometimes LBRY App doesn't take focus, if that is fixed, this can be removed
+  // Close tab if it lacks history and go back if it does
+  chrome.tabs.executeScript(tabId, {
+    code: `if (window.history.length === 1) {
+            window.close();
+          } else {
+            window.history.back();
+          }`
+  });
+}
+
+async function resolveYT(ytUrl: string) {
+  const descriptor = ytService.getId(ytUrl);
+  if (!descriptor) return; // can't parse YT url; may not be one
+
+  const lbryProtocolUrl: string | null = await ytService.resolveById(descriptor).then(a => a[0]);
+  const segments = parseProtocolUrl(lbryProtocolUrl || '', { encode: true });
+  if (segments.length === 0) return;
+  return segments.join('/');
+}
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, { url: tabUrl }) => {
   const { enabled, redirect } = await getSettingsAsync('enabled', 'redirect');
   const urlPrefix = redirectDomains[redirect].prefix;
 
   if (!enabled || !changeInfo.url || !tabUrl) return;
-  if (tabUrl.match(/\b(https:\/\/lbry.tv|lbry:\/\/)/g)) {
 
-    var redirectTo;
-    if (redirect === "app") {
-      let isChannel = tabUrl.match(/^(https|http):\/\/lbry.tv\/@([^?:$#@;/"<>%{}|^~[\]`])+?:[a-z0-9]{1,40}($|(?=\?))/g);
-      let isClaim = tabUrl.match(/^(https|http):\/\/lbry.tv\/@([^?:$#@;/"<>%{}|^~[\]`])+?:[a-z0-9]{1,40}\/([^?:$#@;/"<>%{}|^~[\]`])+?:[a-z0-9]{1,40}($|(?=\?))/g);
+  const url = tabUrl.match(/\b(https:\/\/lbry.tv|lbry:\/\/)/g) ? appRedirectUrl(tabUrl, { encode: true })
+    : await resolveYT(tabUrl);
 
-      if (isChannel) {
-        redirectTo = `lbry://${tabUrl.match(/@([^$#@;/"<>%{}|^~[\]`])+?(?=[#:])/g)![0]}#${tabUrl.match(/#([a-z0-9]{40})|:[a-z0-9]($|(?=\?))/g)![0].substr(1)}`;
-      } else if (isClaim) {
-        redirectTo = `lbry://${tabUrl.match(/@([^$#@;/"<>%{}|^~[\]`])+?(?=[#:])/g)}#${tabUrl.match(/(#([a-z0-9]{40})|:[a-z0-9])(?=\/([^$#@;/"<>%{}|^~[\]`])+?(#([a-z0-9]{40})|:[a-z0-9])($|(?=\?)))/g)![0].substr(1)}${tabUrl.match(/\/([^$#@;/"<>%{}|^~[\]`])+?(?=[#:])/g)![0]}#${tabUrl.match(/(#([a-z0-9]{40})|:[a-z0-9])($|(?=\?))/g)![0].substr(1)}`;
-      }
-    }
-
-    if (redirectTo) {
-      chrome.tabs.update(tabId, { url: redirectTo + "?src=watch-on-lbry" });
-      if (redirect === "app") {
-        alert("Opened link in LBRY App!"); // Better for UX since sometimes LBRY App doesn't take focus, if that is fixed, this can be removed
-
-        // Close tab if it lacks history and go back if it does
-        chrome.tabs.executeScript(tabId, {
-          code: `
-                if (window.history.length === 1) {
-                  window.close();
-                } else {
-                  window.history.back();
-                }`
-        });
-      }
-    }
+  if (!url) return;
+  if (redirect === 'app') {
+    openApp(tabId, urlPrefix + url);
     return;
   }
-  const descriptor = ytService.getId(tabUrl);
-  if (!descriptor) return;
-  const title = (await ytService.resolveById(descriptor))[0]
-  if (!title) return;
-  console.log(title);
-
-  let newUrl;
-  if (redirect === "lbry.tv") {
-    newUrl = `${urlPrefix}${title.replace(/^lbry:\/\//, "").replace(/#/g, ":")}?src=watch-on-lbry`;
-  } else if (redirect === "app") {
-    newUrl = `lbry://${title.replace(/^lbry:\/\//, "")}`;
-  }
-  chrome.tabs.update(tabId, { url: newUrl });
+  chrome.tabs.executeScript(tabId, { code: `location.replace("${urlPrefix + url}")` });
 });
