81f1742289e2f24668c1a855d676e5c0f1eb92f7
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-04-22 20:42:57 +0000

    🍙 updating UI

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,88 +1,71 @@
 import { h, render } from 'preact'
 import { useState } from 'preact/hooks'
-import ButtonRadio, { SelectionOption } from '../common/components/ButtonRadio'
-import { loginAndSetNickname } from '../common/crypto'
-import { getTargetPlatfromSettingsEntiries, getYtUrlResolversSettingsEntiries, TargetPlatformName, YTUrlResolverName } from '../common/settings'
+import { getTargetPlatfromSettingsEntiries, getYtUrlResolversSettingsEntiries } from '../common/settings'
 import { setSetting, useLbrySettings } from '../common/useSettings'
+import '../common/common.css'
+import './popup.css'
 import { LbryPathnameCache } from '../common/yt/urlCache'
-import './popup.sass'
+import { loginAndSetNickname } from '../common/crypto'
 
 
 /** Gets all the options for redirect destinations as selection options */
-const platformOptions: SelectionOption[] = getTargetPlatfromSettingsEntiries()
-  .map(([value, { displayName: display }]) => ({ value, display }))
+const targetPlatforms = getTargetPlatfromSettingsEntiries()
 
-const ytUrlResolverOptions: SelectionOption[] = getYtUrlResolversSettingsEntiries()
-  .map(([value, { name: display }]) => ({ value, display }))
+const ytUrlResolverOptions = getYtUrlResolversSettingsEntiries()
 
 function WatchOnLbryPopup() {
   const { redirect, targetPlatform, urlResolver, privateKey, publicKey } = useLbrySettings()
   let [clearingCache, updateClearingCache] = useState(() => false)
 
-  return <div className='container'>
-    <section>
-      <label className='radio-label'>Enable Redirection:</label>
-      <ButtonRadio value={redirect ? 'YES' : 'NO'} options={['YES', 'NO']}
-        onChange={redirect => setSetting('redirect', redirect.toLowerCase() === 'yes')} />
-    </section>
-    <section>
-      <label className='radio-label'>Where would you like to redirect?</label>
-      <ButtonRadio value={targetPlatform} options={platformOptions}
-        onChange={(platform: TargetPlatformName) => setSetting('targetPlatform', platform)} />
-    </section>
-    <section>
-      <label className='radio-label'>Resolve URL with:</label>
-      <ButtonRadio value={urlResolver} options={ytUrlResolverOptions}
-        onChange={(urlResolver: YTUrlResolverName) => setSetting('urlResolver', urlResolver)} />
-    </section>
-    <section>
-      <a onClick={async () => {
-        await LbryPathnameCache.clearAll()
-        alert('Cleared Cache.')
-      }}>
-        <button type='button' className='btn1 button is-primary'>{clearingCache ? 'Clearing Cache...' : 'Clear Cache'}</button>
-      </a>
-    </section>
-    <section>
-      <label className='radio-label'>Madiator Finder:</label>
-      <a href='https://finder.madiator.com/leaderboard' target='_blank'>
-        <button type='button' className='btn1 button is-primary'>Leaderboard</button>
-      </a>
-    </section>
-    <section>
-      <label className='radio-label'>Madiator Finder Profile:</label>
-      {(!privateKey || !publicKey) ?
-        <span class="options">
-          <a onClick={() => loginAndSetNickname()} target='_blank'>
-            <button type='button' className='btn1 button is-primary'>Login and join the Leaderboard</button>
+  return <div className='popup'>
+    <header>
+      <div className='profile'>
+        {publicKey
+          ? <span className='name'>{publicKey}</span>
+          : <a className='button' onClick={() => loginAndSetNickname()}>Login</a>}
+      </div>
+    </header>
+    <main>
+      <section>
+        <label>Pick a mode !</label>
+        <div className='options'>
+          <a onClick={() => setSetting('redirect', true)} className={`button ${redirect ? 'active' : ''}`}>
+            Redirect
           </a>
-          <a onClick={() => loginAndSetNickname()} target='_blank'>
-            <button type='button' className='btn1 button is-primary'>Import Account</button>
+          <a onClick={() => setSetting('redirect', false)} className={`button ${redirect ? '' : 'active'}`}>
+            Show a button
           </a>
-        </span>
-        :
-        <span class="options">
-          <a onClick={() => loginAndSetNickname()} target='_blank'>
-            <button type='button' className='btn1 button is-primary'>Update Nickname</button>
-          </a>
-          <a onClick={() => loginAndSetNickname()} target='_blank'>
-            <button type='button' className='btn1 button is-primary'>Export Account</button>
-          </a>
-          <a onClick={() => loginAndSetNickname()} target='_blank'>
-            <button type='button' className='btn1 button is-primary'>Logout</button>
-          </a>
-          <a onClick={() => loginAndSetNickname()} target='_blank'>
-            <button type='button' className='btn1 button is-primary'>Purge Account</button>
-          </a>
-        </span>
-      }
-    </section>
-    <section>
-      <label className='radio-label'>Other useful tools:</label>
-      <a href='/tools/YTtoLBRY.html' target='_blank'>
-        <button type='button' className='btn1 button is-primary'>Subscriptions Converter</button>
-      </a>
-    </section>
+        </div>
+      </section>
+      <section>
+        <label>Where would you like to redirect ?</label>
+        <div className='options'>
+          {targetPlatforms.map(([name, value]) =>
+            <a onClick={() => setSetting('targetPlatform', name)} className={`button ${targetPlatform === name ? 'active' : ''}`}>
+              {value.displayName}
+            </a>
+          )}
+        </div>
+      </section>
+      <section>
+        <label>Which resolver API you want to use ?</label>
+        <div className='options'>
+          {ytUrlResolverOptions.map(([name, value]) =>
+            <a onClick={() => setSetting('urlResolver', name)} className={`button ${urlResolver === name ? 'active' : ''}`}>
+              {value.name}
+            </a>
+          )}
+        </div>
+        <a onClick={async () => {
+          updateClearingCache(true)
+          await LbryPathnameCache.clearAll()
+          updateClearingCache(false)
+          alert("Cleared Cache!")
+        }} className={`button active ${clearingCache ? 'disabled' : ''}`}>
+          Clear Resolver Cache
+        </a>
+      </section>
+    </main>
   </div>
 }
 
